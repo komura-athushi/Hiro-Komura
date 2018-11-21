472c4acfe4f36f79c618e118e250d56447fa297e
komuraathushi <aa>
2018-11-21 13:27:27 +0900

    a

--- Game/Player.h
+++ Game/Player.h
@@ -61,7 +61,7 @@ public:
 	void Damage(int attack);
 private:
 	GameObj::CSkinModelRender* m_skinModelRender = nullptr;		//スキンモデルレンダラー。
-	std::unique_ptr<GameObj::CCollisionObj> m_collision;        //丸いコリジョン
+    GameObj::CCollisionObj* m_collision;                        //丸いコリジョン
 	CFont m_font;                                               //文字表示クラス
 	Bone* m_bone;                                               //骨
 	PlayerStatus* m_playerstatus;                               //プレイヤーステータスのポインタ
